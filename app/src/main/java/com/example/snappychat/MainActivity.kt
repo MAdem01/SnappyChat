ecdb0eada7014c5fe3d0fd369e8713560deadd77
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-02-28 16:32:04 +0100

    add hasRequiredPermissions fun

--- app/src/main/java/com/example/snappychat/MainActivity.kt
+++ app/src/main/java/com/example/snappychat/MainActivity.kt
@@ -6,13 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.snappychat.ui.theme.SnappyChatTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +19,16 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun hasRequiredPermissions(): Boolean {
+        return CAMERAX_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
     companion object {
         private val CAMERAX_PERMISSIONS = arrayOf(
             Manifest.permission.CAMERA,
