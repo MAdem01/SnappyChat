d164894ebd5a1e9fdd27b96d2c42beadccf3ec9a
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-03-03 12:42:19 +0100

    add imports

--- app/src/main/java/com/example/snappychat/MainActivity.kt
+++ app/src/main/java/com/example/snappychat/MainActivity.kt
@@ -5,13 +5,19 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -31,6 +37,7 @@ class MainActivity : ComponentActivity() {
                 0
             )
         }
+
         setContent {
             SnappyChatTheme {
                 val scaffoldState = rememberBottomSheetScaffoldState()
