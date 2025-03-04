9ab4dcc91622b2571036f6a4c582947ee3edddb6
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-03-04 12:15:56 +0100

    switch currentPhoto variable with photoViewModel

--- app/src/main/java/com/example/snappychat/MainActivity.kt
+++ app/src/main/java/com/example/snappychat/MainActivity.kt
@@ -2,57 +2,22 @@ package com.example.snappychat
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Matrix
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
+import androidx.activity.viewModels
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Cameraswitch
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.outlined.Circle
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.snappychat.cameraActivity.CameraScreen
+import com.example.snappychat.photoReview.PhotoReviewScreen
 import com.example.snappychat.ui.theme.SnappyChatTheme
+import com.example.snappychat.viewModel.PhotoViewModel
 
 class MainActivity : ComponentActivity() {
+    private val photoViewModel : PhotoViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SnappyChatTheme {
-                var currentPhoto by remember { mutableStateOf<Bitmap?>(null) }
+                val currentPhoto = photoViewModel.currentPhoto
                 val controller = remember {
                     LifecycleCameraController(applicationContext).apply {
                         setEnabledUseCases(
@@ -78,156 +43,17 @@ class MainActivity : ComponentActivity() {
                 if(currentPhoto == null) {
                    CameraScreen(
                        controller = controller,
-                       onPhotoTaken = {photo -> currentPhoto = photo}
+                       onPhotoTaken = {photo -> photoViewModel.setPhoto(photo)},
+                       this
                    )
                 }else{
-                    PhotoPreviewScreen(
-                        photo = currentPhoto!!,
-                        onClose = {currentPhoto = null}
+                    PhotoReviewScreen(
+                        photo = currentPhoto,
+                        onClose = {photoViewModel.clearPhoto()}
                     )
                 }
             }
         }
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun CameraScreen(controller: LifecycleCameraController, onPhotoTaken: (Bitmap) -> Unit) {
-        val scaffoldState = rememberBottomSheetScaffoldState()
-
-        BottomSheetScaffold(
-            scaffoldState = scaffoldState,
-            sheetPeekHeight = 0.dp,
-            sheetContent = {  }
-        ) { padding ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding)
-            ) {
-                CameraPreview(controller = controller, modifier = Modifier.fillMaxSize())
-
-                IconButton(
-                    onClick = { toggleCamera(controller) },
-                    modifier = Modifier
-                        .align(Alignment.TopEnd)
-                        .offset((-12).dp, 12.dp)
-                        .height(35.dp)
-                        .width(35.dp)
-                ) {
-                    Icon(imageVector = Icons.Default.Cameraswitch,
-                        contentDescription = "Switch Camera",
-                        tint = Color.White,
-                        modifier = Modifier
-                            .fillMaxSize()
-                    )
-                }
-
-                Column(
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .fillMaxWidth()
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                            .height(100.dp),
-                        horizontalArrangement = Arrangement.SpaceAround
-                    ) {
-                        IconButton(
-                            onClick = { takePhoto(controller, onPhotoTaken) },
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .aspectRatio(1f)
-                        ) {
-                            Icon(
-                                imageVector = Icons.Outlined.Circle,
-                                contentDescription = "Take Photo",
-                                tint = Color.White,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                            )
-                        }
-                    }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                            .background(color = Color.Black),
-                        horizontalArrangement = Arrangement.SpaceAround
-                    ) {
-                    }
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun PhotoPreviewScreen(photo: Bitmap, onClose: () -> Unit) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Image(
-                bitmap = photo.asImageBitmap(),
-                contentDescription = "Photo",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-            IconButton(
-                onClick = { onClose() },
-                modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .offset(12.dp, 12.dp)
-                    .width(35.dp)
-                    .height(35.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Close,
-                    contentDescription = "Close Photo",
-                    tint = Color.White,
-                    modifier = Modifier
-                        .fillMaxSize()
-                )
-            }
-        }
-    }
-
-
-    private fun takePhoto(
-        controller: LifecycleCameraController,
-        onPhotoTaken: (Bitmap) -> Unit
-    ){
-        controller.takePicture(
-            ContextCompat.getMainExecutor(applicationContext),
-            object : ImageCapture.OnImageCapturedCallback(){
-                override fun onCaptureSuccess(image: ImageProxy) {
-                    super.onCaptureSuccess(image)
-                    val bitmap = image.toBitmap()
-
-                    if (controller.cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA) {
-                        val flippedBitmap = Bitmap.createBitmap(
-                            bitmap,
-                            0,
-                            0,
-                            bitmap.width,
-                            bitmap.height,
-                            Matrix().apply {
-                                postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
-                            },
-                            true
-                        )
-                        onPhotoTaken(flippedBitmap)
-                    } else {
-                        onPhotoTaken(bitmap)
-                    }
-                    image.close()
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    super.onError(exception)
-                    Log.e("Camera", "Error taking a picture", exception)
-                }
-            }
-        )
     }
 
     private fun hasRequiredPermissions(): Boolean {
@@ -236,14 +62,6 @@ class MainActivity : ComponentActivity() {
                 applicationContext,
                 it
             ) == PackageManager.PERMISSION_GRANTED
-        }
-    }
-
-    private fun toggleCamera(controller:LifecycleCameraController){
-        if(controller.cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA){
-            controller.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-        }else{
-            controller.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
         }
     }
 
