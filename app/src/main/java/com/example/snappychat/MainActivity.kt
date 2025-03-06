4f5ded1065506792588483ca6ddc1a7bed09c0cf
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-03-06 10:33:14 +0100

    refactor SetContent with cameraViewModel

--- app/src/main/java/com/example/snappychat/MainActivity.kt
+++ app/src/main/java/com/example/snappychat/MainActivity.kt
@@ -6,18 +6,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.camera.view.CameraController
-import androidx.camera.view.LifecycleCameraController
-import androidx.compose.runtime.remember
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.snappychat.cameraActivity.CameraScreen
+import com.example.snappychat.cameraActivity.CameraViewModel
 import com.example.snappychat.photoReview.PhotoReviewScreen
 import com.example.snappychat.ui.theme.SnappyChatTheme
 import com.example.snappychat.viewModel.PhotoViewModel
 
 class MainActivity : ComponentActivity() {
     private val photoViewModel : PhotoViewModel by viewModels()
+    private val cameraViewModel: CameraViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,25 +25,22 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SnappyChatTheme {
-                val currentPhoto = photoViewModel.currentPhoto
-                val controller = remember {
-                    LifecycleCameraController(applicationContext).apply {
-                        setEnabledUseCases(
-                            CameraController.IMAGE_CAPTURE or
-                                CameraController.VIDEO_CAPTURE
-                        )
-                    }
-                }
-                if(currentPhoto == null) {
-                   CameraScreen(
-                       controller = controller,
-                       onPhotoTaken = {photo -> photoViewModel.setPhoto(photo)},
-                       this
-                   )
+                val controller = cameraViewModel.cameraController
+                if(photoViewModel.currentPhoto == null){
+                    CameraScreen(
+                        controller = controller,
+                        onPhotoTaken =
+                            { photo ->
+                            photoViewModel.setPhoto(photo)
+                            },
+                        this
+                    )
                 }else{
                     PhotoReviewScreen(
-                        photo = currentPhoto,
-                        onClose = {photoViewModel.clearPhoto()}
+                        photoViewModel.currentPhoto!!,
+                        onClose = {
+                            photoViewModel.clearPhoto()
+                        }
                     )
                 }
             }
