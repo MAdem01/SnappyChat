09ed1a78824d847b14aee605b670d3292f1be85a
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-03-06 12:45:11 +0100

    add login screen

---
+++ app/src/main/java/com/example/snappychat/LoginScreen.kt
@@ -0,0 +1,146 @@
+package com.example.snappychat
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen(){
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val red = Color(0xFFFF3131)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(red),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement
+                .spacedBy(16.dp),
+            horizontalAlignment = Alignment
+                .CenterHorizontally
+        ){
+            Text(
+                text = "SnappyChat",
+                fontFamily = FontFamily.Cursive,
+                fontStyle = FontStyle.Italic,
+                fontSize = 45.sp,
+                modifier = Modifier
+                    .padding(bottom = 50.dp, top = 30.dp)
+            )
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                )
+                )
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                )
+                )
+            Button(
+                onClick = {},
+                colors = ButtonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White,
+                    disabledContentColor = Color.White,
+                    disabledContainerColor = Color.Black)
+
+            ) {
+                Text(
+                    text = "Login",
+                    modifier = Modifier
+                        .background(color = Color.Black))
+            }
+            Row(
+                modifier = Modifier
+                    .height(40.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("Don't have an account?")
+                Spacer(
+                    modifier = Modifier
+                        .width(4.dp)
+                )
+                Button(
+                    onClick = {},
+                    colors = ButtonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.Blue,
+                        disabledContainerColor = Color.Transparent,
+                        disabledContentColor = Color.Blue
+                    ),
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .width(50.dp),
+                    contentPadding = PaddingValues(0.dp)
+
+
+                ) {
+                    Text(
+                        text = "Sign up",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
+}
